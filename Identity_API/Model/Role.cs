143193fe2b1256a67f40f7d77daec2c397259002
janstigliani <janstigli@gmail.com>
2025-06-13 13:17:50 +0200

    aggiungi Role e UserRole con relazione molti a molti al db

---
+++ Identity_API/Model/Role.cs
@@ -0,0 +1,10 @@
+﻿namespace Identity_API.Model
+{
+    public class Role
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+
+    }
+}
---
+++ Identity_API/Model/UserRole.cs
@@ -0,0 +1,10 @@
+﻿namespace Identity_API.Model
+{
+    public class UserRole
+    {
+        public int UserId { get; set; }
+        public int RoleId { get; set; }
+        public DateTime StartDate { get; set; } 
+        public DateTime? EndDate { get; set; } 
+    }
+}
