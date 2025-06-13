950c4f45cefc233b222d610935bab63a8ec5b00d
janstigliani <janstigli@gmail.com>
2025-06-13 13:18:57 +0200

    Tabella molti a molti User => UserRole => Role da aggiungere a DB

--- Identity_API/Model/UserRole.cs
+++ Identity_API/Model/UserRole.cs
@@ -2,6 +2,7 @@
 {
     public class UserRole
     {
+        public int Id { get; set; }
         public int UserId { get; set; }
         public int RoleId { get; set; }
         public DateTime StartDate { get; set; } 
